a3784e7146131d67974eceee482faeb26b4886b7
Harsh-cal <harsh.jha@calsoftinc.com>
2025-07-23 15:08:49 +0530

    Upto the blog page hand over to ajay

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+
+import React from 'react'
+import './App.css'
+import Hero from './components/Hero'
+import Footer from './utils/Footer'
+import { Route, Routes } from 'react-router-dom'
+import Header from './utils/Header'
+import Blog from './components/BlogPosts/Blog'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Hero />} />
+ <Route path="/blogs" element={<Blog />} />
+
+        {/* aur bhi route yahan aa sakte hain */}
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
+
+
+
+//   return (
+//   <>
+// {/* <div className='mt-[110px]'></div> */}
+//     <Header  />
+//       <Hero  />
+
+//       <Footer />
+//   </>
+//   )
+// }
+
